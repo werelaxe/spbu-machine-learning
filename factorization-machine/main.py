da8be5ec1280ecbb2cda9500aa1cb472a3909c85
Daniil Sharko <daniil.sharko@jetbrains.com>
2020-01-20 11:40:08 +0300

    Tiny fixes

--- factorization-machine/main.py
+++ factorization-machine/main.py
@@ -28,8 +28,8 @@ def get_grads(xs, ys, vector_w, w0, matrix_v):
     delta = ys - value
     grad_w0 = 2 * np.sum(delta) / xs.shape[1]
     grad_w = 2 * (xs.T.dot(delta)) / xs.shape[1]
-    grad_v = 2 * ((xs.T.dot(np.multiply(delta, xs.dot(matrix_v)))) - np.multiply(matrix_v, xs.T.power(2).dot(delta))) / \
-             xs.shape[1]
+    grad_v = 2 * ((xs.T.dot(np.multiply(delta, xs.dot(matrix_v))))
+                  - np.multiply(matrix_v, xs.T.power(2).dot(delta))) / xs.shape[1]
     return grad_w0, grad_w, grad_v
 
 
@@ -39,28 +39,24 @@ def do_step(batch_xs, batch_ys, vector_w, w0, matrix_v, learning_rate):
 
 
 def extract_batch(xs, ys, batch_index, batch_size):
-    return xs.tocsr()[batch_index * batch_size: (batch_index + 1) * batch_size, :], ys[batch_index * batch_size: (
-                                                                                                                         batch_index + 1) * batch_size,
-                                                                                    :]
+    return xs.tocsr()[batch_index * batch_size: (batch_index + 1) * batch_size, :], \
+           ys[batch_index * batch_size: (batch_index + 1) * batch_size, :]
 
 
 def learn(xs, ys, k, learning_rate, iters_count, epoch_count, batch_size):
     vector_w = np.random.rand(xs.shape[1], 1)
     w0 = random.random()
 
-    matrix_v = np.full((xs.shape[1], k), 0.5)
+    matrix_v = np.random.rand(xs.shape[1], k)
 
     rmse_val = 99999999999
     for iter_index in range(iters_count):
-        # print("iters_count", iter_index)
-        for epoch_index in range(epoch_count):
-            # print("epoch_index", epoch_index)
-            xs, ys = shuffle(xs, ys)
-            number_of_batches = xs.shape[0] // batch_size
+        xs, ys = shuffle(xs, ys)
+        number_of_batches = xs.shape[0] // batch_size
 
-            for batch_index in range(number_of_batches):
-                batch_xs, batch_ys = extract_batch(xs, ys, batch_index, batch_size)
-                w0, vector_w, matrix_v = do_step(batch_xs, batch_ys, vector_w, w0, matrix_v, learning_rate)
+        for batch_index in range(number_of_batches):
+            batch_xs, batch_ys = extract_batch(xs, ys, batch_index, batch_size)
+            w0, vector_w, matrix_v = do_step(batch_xs, batch_ys, vector_w, w0, matrix_v, learning_rate)
         rmse_val = rmse(vector_w, w0, matrix_v, xs, ys)
         if rmse_val < 10e-5:
             break
@@ -70,8 +66,8 @@ def learn(xs, ys, k, learning_rate, iters_count, epoch_count, batch_size):
 
 
 def main():
-    xs, ys, encoder = read_dataset()
-    _, _, _, rmse_val = learn(xs, ys, 3, 0.01, 10000, 1, xs.shape[0])
+    xs, ys, encoder = read_dataset(10000)
+    _, _, _, rmse_val = learn(xs, ys, 3, 0.1, 10000, 1, xs.shape[0])
     print(rmse_val)
 
 
