cbc7a01adad4a4204be629c7c5c2bf743ffe6dd9
Daniil Sharko <daniil.sharko@jetbrains.com>
2020-01-20 10:04:28 +0300

    Optimize FM

--- factorization-machine/main.py
+++ factorization-machine/main.py
@@ -1,96 +1,78 @@
 import random
-from time import time
 
 import numpy as np
-from scipy.sparse import csr_matrix
+from sklearn.utils import shuffle
 
-from one_hot_synthetics import get_dataset_with_encoder
+from dataset_reader import read_dataset
 
 FACTORS_COUNT = 3
-random.seed(0)
 
 
-def compute_f(vector_w, matrix_v, xs):
-    s = np.zeros(xs.shape[0])
-    for f in range(len(matrix_v[0])):
-        x = xs.multiply(matrix_v[:, f])
-        s = s + np.transpose(np.square(x.sum(axis=1)) - x.power(2).sum(axis=1))
-    return vector_w[0] + xs.dot(vector_w[1:]) + s / 2
+def compute_f(vector_w, w0, matrix_v, xs):
+    vx = xs.dot(matrix_v)
+    vx = (vx ** 2) - xs.power(2).dot(matrix_v ** 2)
+    add_v = (np.sum(vx, axis=1) / 2).reshape(-1, 1)
+    vx = xs.dot(vector_w)
+    vx = w0 + vx
+    return vx + add_v
 
 
-def mse(xs, ys, vector_w, matrix_v):
-    f_xs = compute_f(vector_w, matrix_v, xs)
-    return (np.square(ys - f_xs)).sum() / xs.shape[0]
+def rmse(vector_w, w0, matrix_v, xs, ys):
+    pre = compute_f(vector_w, w0, matrix_v, xs)
+    mse = np.sum((ys - pre) ** 2) / pre.shape[1]
+    return np.sqrt(mse)
 
 
-def r2(xs, ys, vector_w, matrix_v):
-    avg = np.average(ys)
-    f_xs = compute_f(vector_w, matrix_v, xs)
-    ss_res = ((ys - f_xs) ** 2).sum()
-    ss_tot = ((ys - avg) ** 2).sum()
-    return 1 - ss_res / ss_tot
+def get_grads(xs, ys, vector_w, w0, matrix_v):
+    value = compute_f(vector_w, w0, matrix_v, xs)
+    delta = ys - value
+    grad_w0 = 2 * np.sum(delta) / xs.shape[1]
+    grad_w = 2 * (xs.T.dot(delta)) / xs.shape[1]
+    grad_v = 2 * ((xs.T.dot(np.multiply(delta, xs.dot(matrix_v)))) - np.multiply(matrix_v, xs.T.power(2).dot(delta))) / \
+             xs.shape[1]
+    return grad_w0, grad_w, grad_v
 
 
-def rmse(xs, ys, vector_w, matrix_v):
-    return np.sqrt(mse(xs, ys, vector_w, matrix_v))
+def do_step(batch_xs, batch_ys, vector_w, w0, matrix_v, learning_rate):
+    grad_w0, grad_w, grad_v = get_grads(batch_xs, batch_ys, vector_w, w0, matrix_v)
+    return w0 + grad_w0 * learning_rate, vector_w + grad_w * learning_rate, matrix_v + grad_v * learning_rate
 
 
-def grad_of_mse_ws(xs, ys, vector_w):
-    value = xs.dot(vector_w[1:]) - ys + vector_w[0]
-    pre_result = np.insert(csr_matrix(value).dot(xs).todense(), 0, value.sum())
-    result = 2 * pre_result / xs.shape[0]
-    return np.squeeze(np.asarray(result))
+def extract_batch(xs, ys, batch_index, batch_size):
+    return xs.tocsr()[batch_index * batch_size: (batch_index + 1) * batch_size, :], ys[batch_index * batch_size: (
+                                                                                                                         batch_index + 1) * batch_size,
+                                                                                    :]
 
 
-def grad_of_mse_by_f_l(xs, ys, vector_w, matrix_v, f, l):
-    v = matrix_v[:, f]
-    vx = csr_matrix(xs.dot(v))
-    q = compute_f(vector_w, matrix_v, xs) - ys
-    w = vx.multiply(xs[:, l].transpose()) - (v[l] * xs[:, l].power(2)).transpose()
+def learn(xs, ys, k, learning_rate, iters_count, epoch_count, batch_size):
+    vector_w = np.random.rand(xs.shape[1], 1)
+    w0 = random.random()
 
-    q = np.squeeze(np.asarray(q))
-    w = np.squeeze(np.asarray(w.todense()))
+    matrix_v = np.full((xs.shape[1], k), 0.5)
 
-    return 2 * (q * w).sum() / xs.shape[0]
+    rmse_val = 99999999999
+    for iter_index in range(iters_count):
+        # print("iters_count", iter_index)
+        for epoch_index in range(epoch_count):
+            # print("epoch_index", epoch_index)
+            xs, ys = shuffle(xs, ys)
+            number_of_batches = xs.shape[0] // batch_size
 
+            for batch_index in range(number_of_batches):
+                batch_xs, batch_ys = extract_batch(xs, ys, batch_index, batch_size)
+                w0, vector_w, matrix_v = do_step(batch_xs, batch_ys, vector_w, w0, matrix_v, learning_rate)
+        rmse_val = rmse(vector_w, w0, matrix_v, xs, ys)
+        if rmse_val < 10e-5:
+            break
+        print(rmse_val)
 
-def grad_of_mse_v(xs, ys, vector_w, matrix_v):
-    a = []
-    for f in range(FACTORS_COUNT):
-        b = []
-        for l in range(xs.shape[1]):
-            b.append(grad_of_mse_by_f_l(xs, ys, vector_w, matrix_v, f, l))
-        a.append(b)
-    return np.transpose(np.array(a))
-
-
-def get_value():
-    return random.random()
+    return vector_w, w0, matrix_v, rmse_val
 
 
 def main():
-    xs, ys, encoder = get_dataset_with_encoder(sparse=True)
-    n = xs.shape[0]
-    m = xs.shape[1]
-    vector_w = np.array([random.random() for _ in range(m + 1)])
-    matrix_v = np.array([[1. for _ in range(FACTORS_COUNT)] for _ in range(m)])
-
-    SIZE = 20
-    k = 0.01
-    start = time()
-
-    rmse_value = 99999999999
-    while True:
-        try:
-            grad_of_v = grad_of_mse_v(xs, ys, vector_w, matrix_v)
-            matrix_v -= grad_of_v * k
-            grad_of_w = grad_of_mse_ws(xs, ys, vector_w)
-            vector_w -= grad_of_w * k
-            rmse_value = rmse(xs, ys, vector_w, matrix_v)
-            print(rmse_value)
-        except KeyboardInterrupt:
-            break
-    print("rmse:", rmse(xs, ys, vector_w, matrix_v))
+    xs, ys, encoder = read_dataset()
+    _, _, _, rmse_val = learn(xs, ys, 3, 0.01, 10000, 1, xs.shape[0])
+    print(rmse_val)
 
 
 if __name__ == '__main__':
