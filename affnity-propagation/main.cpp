9f184f2d3958621aada9a3bf71dd678925a51ea1
Daniil Sharko <daniil.sharko@jetbrains.com>
2020-01-27 01:38:40 +0300

    Rewrite sparse method

--- affnity-propagation/main.cpp
+++ affnity-propagation/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
@@ -6,177 +7,220 @@
 
 using namespace std;
 
-namespace std {
-    template<>
-    struct hash<std::pair<int, int>> {
-        inline size_t operator()(const std::pair<int, int> &v) const {
-            std::hash<int> int_hasher;
-            return int_hasher(v.first) ^ int_hasher(v.second);
+
+template <typename T>
+struct SparseMatrix {
+    T get(int i, int k) {
+        if (data.find(i) != data.end()) {
+            if (data[i].find(k) != data[i].end()) {
+                return data[i][k];
+            }
         }
-    };
-}
-
-using dict = unordered_map<pair<int, int>, double>;
-
-
-struct Triple {
-    double first, second, third;
-
-    bool operator==(const Triple& other) const {
-        return other.first == first && other.second == second && other.third == third;
+        return T();
     }
+
+    void set(int i, int k, T value) {
+        data[i][k] = value;
+    }
+
+    unordered_map<int, unordered_map<int, T>> data;
 };
 
 
-void print_dict(dict& s, int sz) {
+void print_dict(SparseMatrix<double>& s, int sz) {
     for (int i = 0; i < sz; ++i) {
         for (int j = 0; j < sz; ++j) {
-            if (s[make_pair(i, j)] > 0) {
-                cout << " " << s[make_pair(i, j)] << " ";
-            } else if (s[make_pair(i, j)] < 0) {
-                cout << s[make_pair(i, j)] << " ";
+            double value = s.get(i, j);
+            if (value > 0) {
+                cout << " " << value << " ";
+            } else if (value < 0) {
+                cout << value << " ";
             } else {
                 cout << " 0 ";
             }
         }
         cout << endl;
     }
-}
-
-
-void iteration(dict& a, dict& s, dict& r, int size) {
-    unordered_map<int, pair<double, double>> double_maxes;
-    pair<double, double > zero_pair(0., 0.);
-    pair<double, double > default_maxes(-(double)INFINITY, -(double)INFINITY);
-
-//    cout << 1 << endl;
-    // pre-compute maxes for (1)
-    for (auto& edge_pair: s) {
-        auto& maxes = double_maxes[edge_pair.first.first];
-        auto edge_value = a[edge_pair.first] + edge_pair.second;
-
-        if (maxes == zero_pair) {
-            maxes = default_maxes;
-        }
-        if (edge_value >= maxes.first) {
-            maxes.second = maxes.first;
-            maxes.first = edge_value;
-        } else if (edge_value > maxes.second) {
-            maxes.second = edge_value;
-        }
-    }
-
-//    cout << 2 << endl;
-    // update matrix R (1)
-    for (auto& edge_pair: s) {
-        auto& maxes = double_maxes[edge_pair.first.second];
-        auto edge_value = a[edge_pair.first] + edge_pair.second;
-
-        double max_value;
-
-        if (edge_value < maxes.first) {
-            max_value = maxes.first;
-        } else {
-            max_value = maxes.second;
-        }
-        r[edge_pair.first] = edge_pair.second - max_value;
-    }
-
-    unordered_map<int, double> sums;
-
-//    cout << 3 << endl;
-    // pre-compute sums for (2)
-    for (auto& edge_pair: s) {
-        sums[edge_pair.first.second] += max(0., r[edge_pair.first]);
-    }
-
-//    cout << 4 << endl;
-    // update matrix A (2)
-    int count = 0;
-    for (auto& edge_pair: s) {
-        if (edge_pair.first.first == edge_pair.first.second) {
-            continue;
-        }
-//        cout << count++ / (float)s.size() << endl;
-        a[edge_pair.first] = min(0., r[make_pair(edge_pair.first.second, edge_pair.first.second)] + sums[edge_pair.first.second] - r[edge_pair.first]);
-//        cout << "new val: " << r[make_pair(edge_pair.first.second, edge_pair.first.second)] << " " << a[edge_pair.first] << endl;
-    }
-
-    sums.clear();
-
-//    cout << 5 << endl;
-    // pre-compute sums for (3)
-    for (auto& edge_pair: s) {
-        if (edge_pair.first.first == edge_pair.first.second) {
-            continue;
-        }
-        sums[edge_pair.first.second] += max(0., r[edge_pair.first]);
-    }
-
-//    cout << 6 << endl;
-    for (int k = 0; k < size; ++k) {
-        a[make_pair(k, k)] = sums[k];
-    }
-}
-
-
-vector<pair<int, double>> get_result(dict& a, dict& s, dict& r, int size) {
-    vector<pair<int, double>> c(size, make_pair(-1, -(double)INFINITY));
-
-    for (auto& edge_pair: s) {
-        int i = edge_pair.first.first;
-        int k = edge_pair.first.second;
-
-        double value = a[edge_pair.first] + r[edge_pair.first];
-
-        if (value > c[i].second) {
-            c[i].second = value;
-            c[i].first = k;
-        }
-    }
-
-    return c;
-}
-
-
-void print_result(vector<pair<int, double>>& result) {
-    for (auto& pair: result) {
-        cout << pair.first << " ";
-    }
     cout << endl;
 }
 
 
-int main () {
-//    srand(time(nullptr));
-    dict a, s, r;
+void iteration(SparseMatrix<double>& a, SparseMatrix<double>& s, SparseMatrix<double>& r, int size) {
+    unordered_map<int, pair<double, double>> double_maxes;
+    pair<double, double> zero_pair(0., 0.);
+    pair<double, double> default_maxes(-(double) INFINITY, -(double) INFINITY);
 
-    const int size = 10;
-    int edges_count = 10;
-    auto start = time(nullptr);
+    cout << 1 << endl;
 
-    for (int i = 0; i < edges_count; i++) {
-        int x = rand() % size;
-        int y = rand() % size;
-        s[make_pair(x, y)] = 1.;
-        s[make_pair(y, x)] = 1.;
-        s[make_pair(0, x)] = 1.;
-        s[make_pair(x, 0)] = 1.;
+    for (auto& row: s.data) {
+        for (auto& pair: row.second) {
+            int j = row.first;
+            int k = pair.first;
+
+            auto& maxes = double_maxes[j];
+            auto edge_value = a.get(j, k) + pair.second;
+
+            if (maxes == zero_pair) {
+                maxes = default_maxes;
+            }
+//            cout << "(" << i << ", " << k << ") ev: " << edge_value << ", fm: " << maxes.first << ", sm: " << maxes.second << endl;
+
+            if (edge_value >= maxes.first) {
+                maxes.second = maxes.first;
+                maxes.first = edge_value;
+            } else if (edge_value > maxes.second) {
+                maxes.second = edge_value;
+            }
+        }
     }
 
+//    for (int i = 0; i < 10; ++i) {
+//        cout << i << " " << double_maxes[i].first << " " << double_maxes[i].second << endl;
+//    }
+
+    cout << 2 << endl;
+
+//    exit(0);
+//    int count = 0;
+    // update matrix R (1)
+    for (auto& row: s.data) {
+        for (auto& pair: row.second) {
+            int i = row.first;
+            int k = pair.first;
+
+            auto& maxes = double_maxes[i];
+            auto edge_value = a.get(i, k) + pair.second;
+
+            double max_value;
+
+            if (edge_value < maxes.first) {
+                max_value = maxes.first;
+            } else {
+                max_value = maxes.second;
+            }
+            r.set(i, k, pair.second - max_value);
+        }
+    }
+
+    unordered_map<int, double> sums;
+
+    cout << 3 << endl;
+    // pre-compute sums for (2)
+    for (auto& row: s.data) {
+        for (auto& pair: row.second) {
+            int j = row.first;
+            int k = pair.first;
+
+            sums[k] += max(0., r.get(j, k));
+        }
+    }
+
+    cout << 4 << endl;
+    // update matrix A (2)
+    for (auto& row: s.data) {
+        for (auto& pair: row.second) {
+            int i = row.first;
+            int k = pair.first;
+
+            if (i == k) {
+                continue;
+            }
+
+            a.set(i, k,min(0., r.get(k, k) + sums[k] - r.get(i, k) - r.get(k, k)));
+        }
+    }
+
+    sums.clear();
+
+    cout << 5 << endl;
+    // pre-compute sums for (3)
+    for (auto&  row: s.data) {
+        for (auto& pair: row.second) {
+            int j = row.first;
+            int k = pair.first;
+
+            if (j == k) {
+                continue;
+            }
+            sums[k] += max(0., r.get(j, k));
+        }
+    }
+
+    cout << 6 << endl;
+    for (int k = 0; k < size; ++k) {
+        a.set(k, k, sums[k]);
+    }
+}
+
+
+vector<pair<int, double>> get_result(SparseMatrix<double>& a, SparseMatrix<double>& s, SparseMatrix<double>& r, int size) {
+    vector<pair<int, double>> c(size, make_pair(-1, -(double)INFINITY));
+    for (auto& row: s.data) {
+        for (auto& pair: row.second) {
+            int i = row.first;
+            int k = pair.first;
+
+            double value = a.get(i, k) + r.get(i, k);
+
+            if (value > c[i].second) {
+                c[i].second = value;
+                c[i].first = k;
+            }
+        }
+    }
+    return c;
+}
+
+
+int main () {
+    SparseMatrix<double> a, s, r;
+
+    int size = -1;
+    auto start = time(nullptr);
+
+    fstream dataset_file("../edges.txt", std::ios_base::in);
+    int x;
+    bool flag = false;
+    int buff = -1;
+
+    int count = 0;
+
+    while (dataset_file >> x) {
+        size = max(size, x);
+        if (flag) {
+            s.set(buff, x, 1.);
+            s.set(x, buff, 1.);
+        } else {
+            buff = x;
+        }
+        flag = !flag;
+    }
+
+    size++;
+    cout << "Size: " << size << endl;
+
     for (int i = 0; i < size; ++i) {
-        s[make_pair(i, i)] = -2.;
+        s.set(i, i, -1.5);
     }
 
     cout << "Start AP" << endl;
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 3; ++i) {
         iteration(a, s, r, size);
     }
     auto result = get_result(a, s, r, size);
-    print_result(result);
 
-//    cout << endl;
-    cout << time(nullptr) - start << endl;
+    fstream out_file("../out.txt", ios_base::out);
+
+    for (auto& pair: result) {
+        out_file << pair.first << " ";
+    }
+    out_file << endl;
+
+    out_file.close();
+    dataset_file.close();
+    for (qwd: s.rows()) {
+
+    }
     return 0;
 }
