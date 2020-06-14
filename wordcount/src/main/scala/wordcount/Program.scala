ce4b780ee175e45f8add4f2e662b4bbce6d109c3
Daniil Sharko <integrex@yandex.ru>
2020-06-14 07:51:24 +0300

    Add dataframe usage and result saving

--- wordcount/src/main/scala/wordcount/Program.scala
+++ wordcount/src/main/scala/wordcount/Program.scala
@@ -1,27 +1,36 @@
 package wordcount
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.functions._
 
-import scala.collection.SortedMap
+import scala.collection.{SortedMap, mutable}
 
 object Program {
   def main(args: Array[String]): Unit = {
-    val sc = SparkSession
+    val sparkSession = SparkSession
       .builder()
       .appName("Java Spark SQL")
       .config("spark.master", "local")
       .getOrCreate()
-      .sparkContext
+    val sc = sparkSession.sparkContext
 
     val file = sc.textFile("hdfs://localhost:9000/user/root/input/man.txt")
-    file.flatMap(line => line.split(" "))
-      .filter(word => word.nonEmpty)
+    val stopWords = new mutable.HashSet() ++ new StopWordsRemover().getStopWords
+
+    val result = file.flatMap(line => line.split("[,.!?:;\\s]"))
+      .filter(word => word.length >= 2 && !stopWords.contains(word))
       .map(word => (word.toLowerCase, 1))
       .reduceByKey(_ + _)
-      .sortBy(_._2, ascending = false)
-      .take(100)
-      .foreach { pair =>
-        println(s"${pair._1}: ${pair._2}")
-      }
+
+    val dataFrame = sparkSession
+      .createDataFrame(result)
+      .toDF("word", "count")
+      .sort(desc("count"))
+      .limit(100)
+
+    dataFrame
+      .write
+      .csv("result")
   }
 }
